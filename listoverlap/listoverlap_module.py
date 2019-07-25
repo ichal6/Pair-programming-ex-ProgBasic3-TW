da0945cc6957e5d75f64ea5fa238ed610aeca974
kemel3 <kemelZX@gmail.com>
2019-07-25 10:12:42 +0200

    End FizzBuzz

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,5 +1,14 @@
 def listoverlap(list1, list2):
-    return
+    list3 = []
+    for el1 in list1:
+        for el2 in list2:
+            if el1 == el2:
+                list3.append(el1)
+    
+    list3 = set(list3)
+    list3 = list(list3)
+    
+    return list3
 
 
 def main():
