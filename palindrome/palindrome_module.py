0ee6acc8f4088c788cbe2eba79dc630446e39fae
kemel3 <kemelZX@gmail.com>
2019-07-25 10:33:08 +0200

    End palidrome

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,10 +1,17 @@
 def palindrome(str):
-    return
-
+    str = str.lower()
+    riverse_str = str[::-1]
+    str = str.split(' ')
+    riverse_str = riverse_str.split(' ')
+    str = "".join(str)
+    riverse_str = "".join(riverse_str)
+    if str == riverse_str:
+        return True
+    else:
+        return False
 
 def main():
     return
-
 
 if __name__ == '__main__':
     main()
